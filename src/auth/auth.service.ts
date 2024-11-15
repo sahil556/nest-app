9f6762413b1915b16900d4ebd8283c753ca6b8da
sahil nandaniya <snandaniya@hhaexchange.com>
2024-11-15 17:00:11 +0530

    Basic password validation added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly usersService: UsersService){}
+
+    async signIn(username: string, pass: string): Promise<any>{
+        const user = await this.usersService.findOne(username);
+
+        // must have some short of password encryption using salt and validation 
+        if(user?.password !== pass)
+        {
+            throw new UnauthorizedException();
+        }
+        const {password, ...rest} = user;
+        return rest;
+    }
+}
---
+++ src/auth/dto/signin-dto.dto.ts
@@ -0,0 +1,4 @@
+export class SignInDto{
+    userName: string;
+    password: string
+}
