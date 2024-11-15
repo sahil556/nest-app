6c0050e88f9edbb45e85b220ad18a0d9d795e1b8
sahil nandaniya <snandaniya@hhaexchange.com>
2024-11-15 17:16:29 +0530

    JWT token generation added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly usersService: UsersService){}
+    constructor(private readonly usersService: UsersService, private jwtService: JwtService){}
 
-    async signIn(username: string, pass: string): Promise<any>{
+    async signIn(username: string, pass: string): Promise<{access_token: string}>{
         const user = await this.usersService.findOne(username);
 
         // must have some short of password encryption using salt and validation 
@@ -13,7 +14,11 @@ export class AuthService {
         {
             throw new UnauthorizedException();
         }
-        const {password, ...rest} = user;
-        return rest;
+
+        const payload = { sub: user.id, username: user.userName };
+
+        return {
+            access_token: await this.jwtService.signAsync(payload)
+        };
     }
 }
